6717f914a220e1577d57d01c9336c9ab451dd132
Filip Hracek <filip.hracek@gmail.com>
2020-05-18 15:54:42 -0700

    Add a bit to populate the networking tab

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:performance_test_app/src/amber.dart';
 import 'package:performance_test_app/src/black.dart';
@@ -12,7 +15,19 @@ import 'package:performance_test_app/src/pink.dart';
 import 'package:performance_test_app/src/welcome.dart';
 import 'package:performance_test_app/src/yellow.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  // Just something to have in the networking tab.
+  HttpClient client = HttpClient();
+  Timer.periodic(const Duration(seconds: 10), (_) async {
+    var request = await client.getUrl(
+      Uri.parse("http://www.example.com/"),
+    );
+    var response = await request.close();
+    print(response.statusCode);
+  });
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
